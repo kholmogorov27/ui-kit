b10f0cfc584854f52daa4b56c80e13f411e0da37
kholmogorov27 <kholmogorov27@gmail.com>
2023-05-22 17:49:43 +0300

    added navbar

--- projects/main/src/app/app.component.ts
+++ projects/main/src/app/app.component.ts
@@ -1,10 +1,74 @@
 import { Component } from '@angular/core';
+import {
+  faFolder,
+  faLemon,
+  faMap,
+  faMoneyBill1,
+  faObjectUngroup,
+  faRectangleList,
+  faSquareCheck,
+  faStar,
+  faUser,
+  faWindowMaximize,
+} from '@fortawesome/free-regular-svg-icons';
+import { Link } from 'ui-kit-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'main';
+  paths: Link[] = [
+    {
+      name: 'Продукты',
+      path: 'products',
+      icon: faLemon,
+    },
+    {
+      name: 'Пользователи',
+      path: 'users',
+      icon: faUser,
+    },
+    {
+      name: 'Категории',
+      path: 'categories',
+      icon: faFolder,
+    },
+    {
+      name: 'Города',
+      path: 'cities',
+      icon: faMap,
+    },
+    {
+      name: 'Бренды',
+      path: 'brands',
+      icon: faStar,
+    },
+    {
+      name: 'Протоколы',
+      path: 'protocols',
+      icon: faObjectUngroup,
+    },
+    {
+      name: 'Заказы',
+      path: 'orders',
+      icon: faSquareCheck,
+    },
+    {
+      name: 'Баннеры',
+      path: 'banners',
+      icon: faWindowMaximize,
+    },
+    {
+      name: 'Семинары',
+      path: 'seminars',
+      icon: faRectangleList,
+    },
+    {
+      name: 'Промокоды',
+      path: 'promocodes',
+      icon: faMoneyBill1,
+    },
+  ];
 }
